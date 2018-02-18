0fff1192fc7fa2fba42f3b894692fb3fbdef2ac6
Hernando Castano <castano.ha@gmail.com>
2018-02-18 05:08:59 -0700

    Add OasisDEX integration migrations (WIP)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,17 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ERC20Events = artifacts.require("ERC20Events");
+var ERC20 = artifacts.require("ERC20");
+var MatchingMarket = artifacts.require("MatchingMarket");
+var MoneyMaker = artifacts.require("MoneyMaker");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(ERC20Events);
+  deployer.link(ERC20Events, ERC20);
+
+  deployer.deploy(ERC20);
+  deployer.link(ERC20, MatchingMarket);
+
+  deployer.deploy(MatchingMarket);
+  deployer.link(MatchingMarket, MoneyMaker);
+
+  deployer.deploy(MoneyMaker);
 };
