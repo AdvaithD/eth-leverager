1dac48b1214e6be94c1c1085950046fc30b853a1
Hernando Castano <castano.ha@gmail.com>
2018-02-18 09:32:59 -0700

    Get MoneyMaker contract to deploy on Kovan

--- contracts/Interface/MatchingMarket.sol
+++ contracts/Interface/MatchingMarket.sol
@@ -5,6 +5,8 @@ import "./ERC20.sol";
 // File found here:
 // https://github.com/makerdao/maker-otc/blob/00cf7de5b8f61ec2bbee73dc64a62fa417d39661/src/matching_market.sol
 contract MatchingMarket {
+  ERC20 token = ERC20(0xd0A1E359811322d97991E03f863a0C30C2cF029C);
+
   function buyAllAmount(ERC20 buy_gem, uint buy_amt, ERC20 pay_gem, uint max_fill_amount) public returns (uint fill_amt);
   function sellAllAmount(ERC20 pay_gem, uint pay_amt, ERC20 buy_gem, uint min_fill_amount) public returns (uint fill_amt);
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,14 +4,5 @@ var MatchingMarket = artifacts.require("MatchingMarket");
 var MoneyMaker = artifacts.require("MoneyMaker");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC20Events);
-  deployer.link(ERC20Events, ERC20);
-
-  deployer.deploy(ERC20);
-  deployer.link(ERC20, MatchingMarket);
-
-  deployer.deploy(MatchingMarket);
-  deployer.link(MatchingMarket, MoneyMaker);
-
   deployer.deploy(MoneyMaker);
 };
