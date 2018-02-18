9d94f412fd8b95efa88d60082ec09da8c85ea499
Alex Tsankov <antsankov@gmail.com>
2018-02-17 21:16:15 -0700

    compiles now

--- contracts/BancorMarketMaker.sol
+++ contracts/BancorMarketMaker.sol
@@ -1,13 +1,13 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.19;
 
 contract IERC20Token {
     // these functions aren't abstract since the compiler emits automatically generated getter functions as external
-    function name() public constant returns (string) {}
-    function symbol() public constant returns (string) {}
-    function decimals() public constant returns (uint8) {}
-    function totalSupply() public constant returns (uint256) {}
-    function balanceOf(address _owner) public constant returns (uint256) { _owner; }
-    function allowance(address _owner, address _spender) public constant returns (uint256) { _owner; _spender; }
+    function name() public constant returns (string);
+    function symbol() public constant returns (string);
+    function decimals() public constant returns (uint8);
+    function totalSupply() public constant returns (uint256);
+    function balanceOf(address _owner) public constant returns (uint256);
+    function allowance(address _owner, address _spender) public constant returns (uint256);
 
     function transfer(address _to, uint256 _value) public returns (bool success);
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
@@ -27,16 +27,16 @@ contract EthLeverage {
     IERC20Token private constant bancorDaiSmartTokenRelay = IERC20Token(0xee01b3AB5F6728adc137Be101d99c678938E6E72);
 
     // gives bancor permission to spend dai on behalf of the contract
-    function approveBancorToSpendDai(uint _value) private returns(uint) {
-        dai.approve(bancorConverterAddress, _value);
+    function approveBancorToSpendDai(uint _value) private returns(bool success) {
+        return dai.approve(bancorConverterAddress, _value);
     }
 
-    // min return is calculated on frontend by taking 95% of bancor sell dai price
-    function sellDaiIntoEth(uint256 _amountDai, uint256 _minReturn) public returns (uint256) {
+    // sell dai price, will be less than normal conversion.
+    function sellDaiForEth(uint256 _amountDai, uint256 _minReturn) public returns (uint256) {
         require(_amountDai > 0);
 
         BancorConverter bancor = BancorConverter(bancorConverterAddress);
-        IERC20Token[] storage daiToEthConversionPath;
+        IERC20Token[] memory daiToEthConversionPath;
         daiToEthConversionPath[0] = dai;
         daiToEthConversionPath[1] = bancorDaiSmartTokenRelay;
         daiToEthConversionPath[2] = bancorDaiSmartTokenRelay;
@@ -47,12 +47,12 @@ contract EthLeverage {
         return bancor.quickConvert(daiToEthConversionPath, _amountDai, _minReturn);
     }
 
-    // min return is calculated on frontend by taking 95% of bancor buy dai price
-    function buyEthIntoDai(uint256 _amountEth, uint256 _minReturn) public returns (uint256) {
+    // buy dai price, will be more than normal conversion, _minReturn should be 1/(Dai/Eth price) * 1.05
+    function buyDaiWithEth(uint256 _amountEth, uint256 _minReturn) public returns (uint256) {
         require(_amountEth > 0);
 
         BancorConverter bancor = BancorConverter(bancorConverterAddress);
-        IERC20Token[] storage ethToDaiConversionPath;
+        IERC20Token[] memory ethToDaiConversionPath;
         ethToDaiConversionPath[0] = bancorErc20Eth;
         ethToDaiConversionPath[1] = bancorToken;
         ethToDaiConversionPath[2] = bancorToken;
